b0d37b3fed78950070e07addb10e479918efe542
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-04 20:09:57 +0300

    создание модели для пользователя

--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
+
+
+class UserCustom(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    first_name = models.CharField(max_length=150, verbose_name='имя', **NULLABLE)
+    last_name = models.CharField(max_length=150, verbose_name='фамилия', **NULLABLE)
+    avatar = models.ImageField(upload_to='Users/', verbose_name='аватар', **NULLABLE)
+
+    role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}: {self.last_name}, {self.first_name}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
