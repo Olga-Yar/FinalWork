347cd1b184b76639f4b53f306e29fa10f3886c38
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-04 20:09:23 +0300

    кастомная команда на создание суперпользователя

---
+++ user/management/commands/csu.py
@@ -0,0 +1,16 @@
+from users.models import UserCustom
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = UserCustom.objects.create(
+            email='test_test@test.ru',
+            first_name='admin',
+            last_name='admin',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password('123098')
+        user.save()
