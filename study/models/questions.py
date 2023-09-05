39516cf05602c13598b86281508ac48fa26124ee
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-05 19:44:50 +0300

    корректировка модели

--- study/models/questions.py
+++ study/models/questions.py
@@ -5,17 +5,16 @@ from rest_framework.exceptions import ValidationError
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Tests(models.Model):
-    test = models.CharField()
-    materials = models.ForeignKey('Materials', verbose_name='материал')
-    time_for_complete = models.TimeField()
-    time_start = models.TimeField(auto_now=True, verbose_name='время начала')
-    is_finished = models.BooleanField(default=False, verbose_name='завершен')
-    correct_answer = models.CharField(verbose_name='верный ответ')
+class Questions(models.Model):
+    title = models.TextField(max_length=255, verbose_name='вопрос', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='опубликован')
+    time_start = models.TimeField(auto_now=True, verbose_name='время старта')
+    time_for_complete = models.TimeField(default='00:01:00', verbose_name='время на выполнение')
+    is_finished = models.BooleanField(default=False, verbose_name='завершено')
 
     def __str__(self):
-        return f'{self.pk}: {self.is_finished}'
+        return f'{self.title}: {self.is_active}, {self.is_finished}'
 
     class Meta:
-        verbose_name = 'раздел'
-        verbose_name_plural = 'разделы'
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
