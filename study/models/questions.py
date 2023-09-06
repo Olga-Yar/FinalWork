e7e7839ae810d7ddb7d416ed626d1f0f89f499cf
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-06 20:19:28 +0300

    дополнение файла readme

--- study/models/questions.py
+++ study/models/questions.py
@@ -8,8 +8,8 @@ NULLABLE = {'blank': True, 'null': True}
 class Questions(models.Model):
     title = models.TextField(max_length=255, verbose_name='вопрос', **NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name='опубликован')
-    time_start = models.TimeField(auto_now=True, verbose_name='время старта')
-    time_for_complete = models.TimeField(default='00:01:00', verbose_name='время на выполнение')
+    # time_start = models.TimeField(auto_now=True, verbose_name='время старта')
+    # time_for_complete = models.TimeField(default='00:01:00', verbose_name='время на выполнение')
     is_finished = models.BooleanField(default=False, verbose_name='завершено')
 
     def __str__(self):
