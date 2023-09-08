2167b52a5b1168615a17abc455aa2f176e4c369e
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-09 01:09:11 +0300

    конект всех моделей в init

---
+++ study/models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'Item',
+    'Materials',
+    'Question',
+    'Answers'
+]
+
+from study.models.answers import Answers
+from study.models.item import Item
+from study.models.materials import Materials
+from study.models.questions import Question
+
