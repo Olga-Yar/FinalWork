42866d051d45c6b4dcb548767c5dce4d82ddc17b
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 23:29:11 +0300

    создание путей

--- study/urls.py
+++ study/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 
 from study.apps import StudyConfig
 from study.views.item import ItemViewSet
+from study.views.materials import MaterialsViewSet
+from study.views.questions import QuestionsViewSet
 
 app_name = StudyConfig.name
 
@@ -11,9 +13,19 @@ urlpatterns = [
     path('item/<int:pk>/', ItemViewSet.as_view({'get': 'retrieve'})),
     path('item/<int:pk>/update/', ItemViewSet.as_view({'put': 'update'})),  # доступ только для модератора
     path('item/create/', ItemViewSet.as_view({'put': 'create'})),   # доступ только для модератора
+
+    path('materials/', MaterialsViewSet.as_view({'get': 'list'})),
+    path('materials/<int:pk>/', MaterialsViewSet.as_view({'get': 'retrieve'})),
+    path('materials/<int:pk>/update/', MaterialsViewSet.as_view({'put': 'update'})),  # доступ только для модератора
+    path('materials/create/', MaterialsViewSet.as_view({'put': 'create'})),   # доступ только для модератора
+
+    path('questions/', QuestionsViewSet.as_view({'get': 'list'})),
+    path('questions/<int:pk>/', QuestionsViewSet.as_view({'get': 'retrieve'})),
+    path('questions/<int:pk>/update/', QuestionsViewSet.as_view({'put': 'update'})),  # доступ только для модератора
+    path('questions/create/', QuestionsViewSet.as_view({'put': 'create'})),   # доступ только для модератора
 ]
 
 router = routers.SimpleRouter()
-router.register('habit', ItemViewSet)
+router.register('study', ItemViewSet)
 
 urlpatterns += router.urls
