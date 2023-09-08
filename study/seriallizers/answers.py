1a48e1c09a4bcfac28ac3765cf6a999441f5a8ec
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-09 01:12:26 +0300

    добавление поля с пользователем

--- study/seriallizers/answers.py
+++ study/seriallizers/answers.py
@@ -4,8 +4,10 @@ from study.models.answers import Answers
 
 
 class AnswersSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Answers
         fields = [
-            'question', 'answer', 'user_answer', 'is_correct_answer', 'is_user_correct',
+            'question', 'answer', 'user_answer', 'is_correct_answer', 'is_user_correct', 'user',
         ]
