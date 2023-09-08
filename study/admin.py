0279da1cd0eb871f0cb581278500363540909c8f
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 22:01:06 +0300

    создание админки для приложения study

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from study.models.answers import Answers
+from study.models.item import Item
+from study.models.materials import Materials
+from study.models.questions import Questions
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'about',
+    )
+
+
+@admin.register(Materials)
+class MaterialsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_m', 'is_finished', 'percent_complete',
+    )
+    list_filter = ('item__name',)
+
+
+@admin.register(Questions)
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'is_active', 'is_finished',
+    )
+    list_filter = ('materials__name_m',)
+
+
+@admin.register(Answers)
+class AnswersAdmin(admin.ModelAdmin):
+    list_display = (
+        'answer', 'user_answer', 'is_correct_answer', 'is_user_correct'
+    )
+    list_filter = ('questions__title',)
