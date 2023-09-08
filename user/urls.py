9c612201c04c8bf4acd19497a7e51b4e3105fc74
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 23:31:32 +0300

    изменение путей для пользователя

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 
-from rest_framework_simplejwt.views import TokenObtainPairView
-from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+from django.urls import path, include
 
 from user.apps import UserConfig
 from user.views import UserCustomViewSet
@@ -8,8 +8,10 @@ from user.views import UserCustomViewSet
 app_name = UserConfig.name
 
 urlpatterns = [
+    path('', include('rest_framework.urls')),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenObtainPairView.as_view()),
+    path('token/verify/', TokenVerifyView.as_view()),
     path('create/', UserCustomViewSet.as_view({'put': 'create'})),
 
 ]
