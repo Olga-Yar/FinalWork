d1edfff33d183ee9ffbc6306da0b33592262a05b
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 22:04:05 +0300

    изменение FK на MtoM для поля question и изменение названия переменной name

--- study/models/materials.py
+++ study/models/materials.py
@@ -7,14 +7,14 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Materials(models.Model):
 
-    name = models.CharField(max_length=50, verbose_name='название', **NULLABLE)
-    question = models.ForeignKey('Questions', on_delete=models.SET_NULL, **NULLABLE) # при удалении - какую зависимость?
+    name_m = models.CharField(max_length=50, verbose_name='название', **NULLABLE)
+    question = models.ManyToManyField('Questions', **NULLABLE) # при удалении - какую зависимость?
     is_finished = models.BooleanField(default=False, verbose_name='завершен')
     count_questions = models.IntegerField(verbose_name='количество вопросов', default=0)
     percent_complete = models.FloatField(verbose_name='процент выполнения', default=0.0)
 
     def __str__(self):
-        return f'{self.name}: {self.percent_complete}'
+        return f'{self.name_m}: {self.percent_complete}'
 
     class Meta:
         verbose_name = 'материал'
