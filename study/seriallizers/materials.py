233e35f59f7f5d1c03860bf3e4840ff2ebcbcef0
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-09 02:08:37 +0300

    создание методов для отображения кол-ва вопросов и доли выполнения

--- study/seriallizers/materials.py
+++ study/seriallizers/materials.py
@@ -5,45 +5,50 @@ from study.models.materials import Materials
 
 class MaterialsSerializer(serializers.ModelSerializer):
     count_questions = serializers.SerializerMethodField()
-    count_right_answers = serializers.SerializerMethodField()
+    # count_right_answers = serializers.SerializerMethodField()
     # answer = serializers.SerializerMethodField()
     percent_complete = serializers.FloatField()
 
+    # def get_num_questions(self, obj):
+    #     """Расчет количества вопросов в данном материале"""
+    #     return obj.question.all().count()
+    #
+    # def get_question(self, obj):
+    #     """Список вопросов в материале"""
+    #     return [i.pk for i in obj.question.all()]
+    #
+    # def get_num_right_answers(self, obj):
+    #     """Получение количества правильных ответов текущего пользователя в данном материале"""
+    #     return obj.answers_set.filter(is_user_correct=True).count()
+    #
+    # def to_representation(self, instance):
+    #     """Вычисление процента выполнения блока. В расчете учитываются только правильные ответа пользователя"""
+    #     representation = super().to_representation(instance)
+    #     representation['percent_complete'] = (
+    #             representation['num_right_answers'] /
+    #             representation['num_questions'] * 100
+    #     )
+    #     return representation
+
     def get_count_questions(self, obj):
         """Расчет количества вопросов в данном материале"""
-        return obj.questions.all().count()
-
-    def get_questions(self, obj):
-        """Список вопросов в материале"""
-        return [i.pk for i in obj.questions.all()]
+        return obj.question.all().count()
 
     def get_right_answers(self, obj):
         """Расчет количества правильных вопросов"""
-        mistakes = 0
-        rights = 0
+        return obj.answers.count()
 
-        for answer in obj.answers.all():
-            if answer.user_answer and answer.is_correct_answer is False:
-                mistakes += 1
-            elif answer.user_answer and answer.is_correct_answer:
-                rights += 1
-            elif answer.user_answer is False and answer.is_correct_answer:
-                mistakes += 1
+    def get_percent_complete(self, obj):
+        """Расчет процента завершенности"""
+        count_questions = obj.question.all().count()
+        count_right_answers = obj.answers.filter(is_user_correct=True).count()
+        if count_questions > 0:
+            return count_right_answers / count_questions * 100
+        return 0
 
-        total = mistakes + rights
-        return rights
-
-    def to_representation(self, instance):
-        """Вычисление процента выполнения блока. В расчете учитываются только правильные ответа пользователя"""
-        representation = super().to_representation(instance)
-        representation['percent_complete'] = (
-                representation['count_right_answers'] /
-                representation['count_questions'] * 100
-        )
-        return representation
 
     class Meta:
         model = Materials
         fields = [
-            'pk', 'name_m', 'questions', 'is_finished', 'count_questions', 'count_right_answers', 'percent_complete',
+            'pk', 'name_m', 'question', 'is_finished', 'count_questions', 'percent_complete',
         ]
