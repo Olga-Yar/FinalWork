660084e454616884379cc8c1e4c8955c0762acd4
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-09 02:08:02 +0300

    создание метода для изменения корректности ответа пользоватлея

--- study/seriallizers/answers.py
+++ study/seriallizers/answers.py
@@ -6,6 +6,10 @@ from study.models.answers import Answers
 class AnswersSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
+    def get_is_user_correct(self, obj):
+        if obj.user_answer and obj.is_correct_answer:
+            return True
+
     class Meta:
         model = Answers
         fields = [
