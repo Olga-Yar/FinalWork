897f67f7b3582774fdbff73c3dad13f9a64781a8
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 22:07:15 +0300

    создание сериализатора для пользователя

--- user/seriallizers.py
+++ user/seriallizers.py
@@ -3,17 +3,26 @@ from rest_framework import serializers
 from user.models import UserCustom
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
-    """Создание пользователя"""
+class UserCustomSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = UserCustom
-        fields = ['email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = [
+            'email', 'first_name', 'last_name', 'avatar', 'role',
+        ]
 
-        def create(self, validated_data):
-            user = UserCustom(
-                email=validated_data['email']
-            )
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
+
+# class CreateUserSerializer(serializers.ModelSerializer):
+#     """Создание пользователя"""
+#     class Meta:
+#         model = UserCustom
+#         fields = ['email', 'password']
+#         extra_kwargs = {'password': {'write_only': True}}
+#
+#         def create(self, validated_data):
+#             user = UserCustom(
+#                 email=validated_data['email']
+#             )
+#             user.set_password(validated_data['password'])
+#             user.save()
+#             return user
