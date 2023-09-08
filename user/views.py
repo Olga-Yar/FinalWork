6199c973d28c0ccd41135c7693058523e823bcbb
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 22:07:59 +0300

    создание эндпоинта для пользователя

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from user.models import UserCustom
+from user.seriallizers import UserCustomSerializer
+
+
+class UserCustomViewSet(ModelViewSet):
+    queryset = UserCustom.objects.all()
+    serializer_class = UserCustomSerializer
+    # permission_classes = [IsAuthenticated, IsModerator]
