926a1fc28de233dd6c88d308544d15fb5970348a
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-08 22:01:42 +0300

    корректировка отображения модели

--- study/models/answers.py
+++ study/models/answers.py
@@ -6,14 +6,13 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Answers(models.Model):
-    question = models.ForeignKey('Questions', on_delete=models.CASCADE)
     answer = models.CharField(max_length=150, verbose_name='ответ', **NULLABLE)
     user_answer = models.BooleanField(default=False, verbose_name='ответ пользователя')
     is_correct_answer = models.BooleanField(default=False, verbose_name='правильный ответ')
     is_user_correct = models.BooleanField(default=False, verbose_name='верный ли ответ пользователя')
 
     def __str__(self):
-        return f'{self.question}: {self.answer} - {self.is_correct_answer}, {self.user_answer}'
+        return f'{self.answer} - {self.is_correct_answer}, {self.user_answer}'
 
     class Meta:
         verbose_name = 'ответ'
