3f3e2b8566c4e518275b26a4201631c8eaddfee6
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-09 16:30:25 +0300

    добавление метода для отображения названий материалов

--- study/seriallizers/item.py
+++ study/seriallizers/item.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
 from study.models.item import Item
+from study.seriallizers.materials import MaterialsSerializer
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    materials_name = serializers.SerializerMethodField()
+
+    def get_materials_name(self, obj):
+        materials = obj.materials.all().values_list('name_m', flat=True)
+        return list(materials)
 
     class Meta:
         model = Item
         fields = [
-            'name', 'about', 'materials', 'user',
+            'name', 'about', 'materials_name',
             ]
