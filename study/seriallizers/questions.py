c4e41dfacac828086754b4cfdc02c4c0ec6b0b14
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-09 16:31:38 +0300

    добавление метода для отображения ответов

--- study/seriallizers/questions.py
+++ study/seriallizers/questions.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from study.models.questions import Question
+from study.seriallizers.answers import AnswersSerializer
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    answer_name = SerializerMethodField()
+
+    def get_answer_name(self, obj):
+        answer = obj.answer.all().values_list('answer', flat=True)
+        return list(answer)
+
     class Meta:
         model = Question
         fields = [
-            'pk', 'title', 'is_active', 'is_finished',
+            'pk', 'title', 'answer_name', 'is_active', 'is_finished',
         ]
