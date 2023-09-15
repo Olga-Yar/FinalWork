fafcbdbac7d3fb9c642dbb55b90dc9f1c8599d97
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 14:58:14 +0300

    фикс бага от тестеровщиков: Создание нового раздела с пустым полем "name" при отправки API запроса - создание валидации на уровне модели

--- study/models/item.py
+++ study/models/item.py
@@ -2,12 +2,14 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from rest_framework.exceptions import ValidationError
 
+from django.utils.translation import gettext_lazy as _
+
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Item(models.Model):
 
-    name = models.CharField(max_length=50, verbose_name='раздел', **NULLABLE)
+    name = models.CharField(max_length=50, verbose_name='раздел', unique=True)
     about = models.TextField(verbose_name='описание', **NULLABLE)
     materials = models.ManyToManyField('Materials')
     user = models.ManyToManyField('user.UserCustom')
@@ -18,3 +20,18 @@ class Item(models.Model):
     class Meta:
         verbose_name = 'раздел'
         verbose_name_plural = 'разделы'
+
+    def save(self, *args, **kwargs):
+        """Валидация имени на уровне модели"""
+        if not self.name.isalpha() or not self.name.isascii() or not self.name.islower():
+            raise ValidationError(
+                _('Имя раздела должно содержать только русские буквы и быть в нижнем регистре.'),
+                code='invalid_russian_name',
+            )
+
+        if not self.name[0].isupper():
+            raise ValidationError(
+                _('Имя раздела должно начинаться с заглавной буквы.'),
+                code='invalid_starts_with_capital',
+            )
+
