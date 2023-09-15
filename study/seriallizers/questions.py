17f3df910788496c85463df0f16b462ee064bb5b
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 14:37:30 +0300

    метода отображения pk в списке ответов

--- study/seriallizers/questions.py
+++ study/seriallizers/questions.py
@@ -7,13 +7,18 @@ from study.seriallizers.answers import AnswersSerializer
 
 class QuestionSerializer(serializers.ModelSerializer):
     answer_name = SerializerMethodField()
+    answer_pk = SerializerMethodField()
 
     def get_answer_name(self, obj):
         answer = obj.answer.all().values_list('answer', flat=True)
         return list(answer)
 
+    def get_answer_pk(self, obj):
+        ans_pk = obj.answer.all().values_list('pk', flat=True)
+        return list(ans_pk)
+
     class Meta:
         model = Question
         fields = [
-            'pk', 'title', 'answer_name', 'is_active', 'is_finished',
+            'pk', 'title', 'answer_pk', 'answer_name', 'is_active', 'is_finished',
         ]
