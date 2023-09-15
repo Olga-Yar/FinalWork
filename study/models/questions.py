b80cfbe821a1171b853dec3c78e0473cbb5d2959
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 14:36:00 +0300

    закомичевание ghjdthrb jndtnf gjkmpjdfntkz на уровне модели

--- study/models/questions.py
+++ study/models/questions.py
@@ -10,6 +10,7 @@ class Question(models.Model):
     is_active = models.BooleanField(default=True, verbose_name='опубликован')
     is_finished = models.BooleanField(default=False, verbose_name='завершено')
     answer = models.ManyToManyField('Answers')
+    # is_user_correct = models.BooleanField(default=False, verbose_name='пользователь прав')
 
     def __str__(self):
         return f'{self.title}: {self.is_active}, {self.is_finished}'
@@ -17,3 +18,13 @@ class Question(models.Model):
     class Meta:
         verbose_name = 'вопрос'
         verbose_name_plural = 'вопросы'
+
+    # def save(self, *args, **kwargs):
+    #     if self.answer.filter(user_answer=True) and self.answer.filter(is_correct_answer=True):
+    #         self.is_user_correct = True
+    #     else:
+    #         self.is_user_correct = False
+    #     # user_correct_answers = self.answer.filter(user_answer=True, is_correct_answer=True).exists()
+    #     # self.is_user_correct = user_correct_answers
+    #
+    #     super().save(*args, **kwargs)
