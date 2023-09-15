a5717003f6196523e85f021d88d7139096052130
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 22:12:42 +0300

    изменение в create PUT на POST

--- study/urls.py
+++ study/urls.py
@@ -13,19 +13,19 @@ urlpatterns = [
     path('item/', ItemViewSet.as_view({'get': 'list'})),
     path('item/<int:pk>/', ItemViewSet.as_view({'get': 'retrieve'})),
     path('item/<int:pk>/update/', ItemViewSet.as_view({'put': 'update'})),  # доступ только для модератора
-    path('item/create/', ItemViewSet.as_view({'put': 'create'})),   # доступ только для модератора
+    path('item/create/', ItemViewSet.as_view({'post': 'create'})),   # доступ только для модератора
     path('item/<int:pk>/delete/', ItemViewSet.as_view({'delete': 'destroy'})),   # доступ только для модератора
 
     path('materials/', MaterialsViewSet.as_view({'get': 'list'})),
     path('materials/<int:pk>/', MaterialsViewSet.as_view({'get': 'retrieve'})),
     path('materials/<int:pk>/update/', MaterialsViewSet.as_view({'put': 'update'})),  # доступ только для модератора
-    path('materials/create/', MaterialsViewSet.as_view({'put': 'create'})),   # доступ только для модератора
+    path('materials/create/', MaterialsViewSet.as_view({'post': 'create'})),   # доступ только для модератора
     path('materials/<int:pk>/delete/', MaterialsViewSet.as_view({'delete': 'destroy'})),   # доступ только для модератора
 
     path('questions/', QuestionViewSet.as_view({'get': 'list'})),
     path('questions/<int:pk>/', QuestionViewSet.as_view({'get': 'retrieve'})),
     path('questions/<int:pk>/update/', QuestionViewSet.as_view({'put': 'update'})),  # доступ только для модератора
-    path('questions/create/', QuestionViewSet.as_view({'put': 'create'})),   # доступ только для модератора
+    path('questions/create/', QuestionViewSet.as_view({'post': 'create'})),   # доступ только для модератора
     path('questions/<int:pk>/delete/', QuestionViewSet.as_view({'delete': 'destroy'})),   # доступ только для модератора
 
     path('answers/<int:pk>/update/', AnswersViewSet.as_view({'put': 'update'})),
