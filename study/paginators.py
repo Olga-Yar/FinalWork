798ffbe23df976e5373e19c35bad4b1a67a926fd
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 14:31:28 +0300

    изменение количества вывода на страницу с 2 на 5

--- study/paginators.py
+++ study/paginators.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ItemPaginator(PageNumberPagination):
-    page_size = 2
+    page_size = 5
     page_query_param = 'page-size'
     max_page_size = 50
