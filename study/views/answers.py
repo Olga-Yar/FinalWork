50d4812c75ef0e03f490d706f4fcd05e1f992c08
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 22:15:57 +0300

    закомичивание метода пермишн

--- study/views/answers.py
+++ study/views/answers.py
@@ -1,16 +1,28 @@
-from rest_framework import status
+from drf_yasg.inspectors import view
+from rest_framework import status, request
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from study.models.answers import Answers
+from study.permissions import IsModerator
 from study.seriallizers.answers import AnswersSerializer
 
 
 class AnswersViewSet(ModelViewSet):
     queryset = Answers.objects.all()
     serializer_class = AnswersSerializer
-    permission_classes = [IsAuthenticated]
+
+    # def get_permissions(self):
+    #     """Проверка разрешения для различных методов"""
+    #     if self.action == 'update' and self.action == 'partial_update':
+    #         permissions_classes = [IsAuthenticated]  # остальные действия только для авторизованных пользователей
+    #     elif self.action == 'create' or self.action == 'update' or self.action == 'destroy':
+    #         permissions_classes = [IsModerator]  # создавать, удалять или обновлять может только модератор
+    #     else:
+    #         permissions_classes = [IsAuthenticated]  # остальные действия только для авторизованных пользователей
+    #
+    #     return [permission() for permission in permissions_classes]
 
     def list(self, request, **kwargs):
         """Отображение списка ответов"""
@@ -31,7 +43,7 @@ class AnswersViewSet(ModelViewSet):
                 serializer.save(user_answer=request.data['user_answer'])
                 self.perform_update(serializer)
             else:
-                return Response({'error': 'Only user_answer field can be updated'},
+                return Response({'error': 'Можно изменить только поле ответ пользователя (user_answer)'},
                                 status=status.HTTP_400_BAD_REQUEST)
             if getattr(instance, '_prefetched_objects_cache', None):
                 instance._prefetched_objects_cache = {}
