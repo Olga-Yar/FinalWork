4bd467f4e6c6e57a0610d1a098416e2adc4c361e
Olga Yarosh <O_ll_y_a@mail.ru>
2023-09-15 22:14:13 +0300

    метод создания

--- user/views.py
+++ user/views.py
@@ -1,5 +1,9 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 from user.models import UserCustom
 from user.seriallizers import UserCustomSerializer
@@ -9,3 +13,13 @@ class UserCustomViewSet(ModelViewSet):
     queryset = UserCustom.objects.all()
     serializer_class = UserCustomSerializer
     # permission_classes = [IsAuthenticated, IsModerator]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+        user = UserCustom.objects.create_user(email=email, password=password)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
